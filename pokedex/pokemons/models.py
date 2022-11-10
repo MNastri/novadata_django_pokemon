02dfc8774f2e188be92891b83ba82636bc654480
MNastri <marcellonastri@gmail.com>
2022-11-10 16:00:58 -0300

    feat:pokemon stats

--- pokedex/pokemons/models.py
+++ pokedex/pokemons/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -7,6 +6,12 @@ class Pokemon(models.Model):
     name = models.CharField(max_length=50, unique=True)
     image = models.ImageField(default="default_missingno.png", upload_to="pokemon_pics")
     number = models.PositiveIntegerField(unique=True)
+    hp = models.PositiveIntegerField(verbose_name="Hit Points")
+    attack = models.PositiveIntegerField()
+    defense = models.PositiveIntegerField()
+    sp_atk = models.PositiveIntegerField(verbose_name="Special Attack")
+    sp_def = models.PositiveIntegerField(verbose_name="Special Defense")
+    speed = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
