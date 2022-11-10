4e077ce21039cf26e4728ed538e79b7b3a15fe87
MNastri <marcellonastri@gmail.com>
2022-11-10 16:01:45 -0300

    fix:profile creation by signals

--- pokedex/users/models.py
+++ pokedex/users/models.py
@@ -3,6 +3,8 @@ import pokemons
 from django.contrib.auth.models import User
 from django.db import models
 
+from . import signals
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,3 +12,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    def create_user_profile(self, sender, instance, created, **kwargs):
+        signals.create_profile(sender, instance, created, **kwargs)
+
+    def save_user_profile(self, sender, instance, **kwargs):
+        signals.save_profile(sender, instance, **kwargs)
